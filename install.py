6e549532708bac1e5c10c7cf819120b24d2414a0
PARADOXHacked <78648401+PARADOXHacked@users.noreply.github.com>
2021-02-07 12:16:36 +0330

    Delete install.py

--- install.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system("unzip PARADOXHacked.zip")
-os.system("pip install colorama")
-os.system("pip install requests")
-os.system("pip install threading")
-os.system("cd PARADOXHacked")
-os.system("python smsbomber.py")
